f53cee53c492a186e6fa9f4d0eb3744cc3867c0d
tunde <ayodeleolumidetunde@gmail.com>
2024-02-10 03:08:44 -0500

    environment variable added

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
-const port = 9000;
+import Products from "./products.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 6000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
 app.get("/api/products", (req, res) => {
-  res.send("product");
+  res.json(Products);
 });
 app.get("/api/products/:id", (req, res) => {
-  res.send("single product");
+  const product = Products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () => console.log(`server running on port ${port}`));

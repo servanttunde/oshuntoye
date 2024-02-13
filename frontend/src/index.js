4666ec3b8603bb191244685dcc2fb18d7264a7ab
tunde <ayodeleolumidetunde@gmail.com>
2024-02-13 16:14:10 -0500

    data management added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./assets/styles/index.css";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
 import "./assets/styles/bootstrap.custom.css";
 import reportWebVitals from "./reportWebVitals";
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
